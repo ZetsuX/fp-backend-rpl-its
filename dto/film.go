28c9854c40c8d236b84ecff366f3b633a7b595e5
Muhammad Hafidh Rosyadi <mhafidhrosyadi@gmail.com>
2023-03-21 18:03:40 +0700

    add CU film

---
+++ dto/film.go
@@ -0,0 +1,31 @@
+package dto
+
+type FilmRegisterRequest struct {
+	Title      string    `json:"title" binding:"required"`
+	Slug       string    `json:"slug" binding:"required"`
+	Synopsis   string    `json:"synopsis" binding:"required"`
+	Duration   int       `json:"duration" binding:"required"`
+	Genre      string    `json:"genre" binding:"required"`
+	Producer   string    `json:"producer" binding:"required"`
+	Director   string    `json:"director" binding:"required"`
+	Writer     string    `json:"writer" binding:"required"`
+	Production string    `json:"production" binding:"required"`
+	Cast       string    `json:"cast" binding:"required"`
+	Trailer    string    `json:"trailer" binding:"required"`
+	Image      string    `json:"image" binding:"required"`
+	Status     string    `json:"status" binding:"required"`
+}
+type FilmUpdateRequest struct {
+	Title      string    `json:"title" binding:"required"`
+	Synopsis   string    `json:"synopsis" binding:"required"`
+	Duration   int       `json:"duration" binding:"required"`
+	Genre      string    `json:"genre" binding:"required"`
+	Producer   string    `json:"producer" binding:"required"`
+	Director   string    `json:"director" binding:"required"`
+	Writer     string    `json:"writer" binding:"required"`
+	Production string    `json:"production" binding:"required"`
+	Cast       string    `json:"cast" binding:"required"`
+	Trailer    string    `json:"trailer" binding:"required"`
+	Image      string    `json:"image" binding:"required"`
+	Status     string    `json:"status" binding:"required"`
+}
---
+++ service/film.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"context"
+	"fp-rpl/dto"
+	"fp-rpl/entity"
+	"fp-rpl/repository"
+
+	"github.com/jinzhu/copier"
+)
+
+type filmService struct {
+	filmRepository repository.FilmRepository
+}
+
+type FilmService interface {
+	CreateNewFilm(ctx context.Context, filmDTO dto.FilmRegisterRequest) (entity.Film, error)
+	GetFilmBySlug(ctx context.Context, slug string) (entity.Film, error)
+	GetAllFilm(ctx context.Context) ([]entity.Film, error)
+	UpdateFilm(ctx context.Context,filmDTO dto.FilmUpdateRequest, slug string) (dto.FilmUpdateRequest, error)
+}
+
+func NewFilmService(filmR repository.FilmRepository) FilmService {
+	return &filmService{filmRepository: filmR}
+}
+func (fs *filmService) CreateNewFilm(ctx context.Context, filmDTO dto.FilmRegisterRequest) (entity.Film, error){
+	filmDTO.Status = "Now playing"
+	var film entity.Film
+	copier.Copy(&film,&filmDTO)
+
+	NewFilm, err := fs.filmRepository.CreateNewFilm(ctx,nil,film)
+	if err != nil{
+		return entity.Film{},err
+	}
+	return NewFilm,nil
+}
+func (fs *filmService) GetFilmBySlug(ctx context.Context, slug string ) (entity.Film, error){
+	film, err := fs.filmRepository.GetFilmBySlug(ctx,nil,slug)
+	if err != nil{
+		return entity.Film{},err
+	}
+	return film,nil
+}
+
+func (fs *filmService) GetAllFilm(ctx context.Context) ([]entity.Film, error){
+	films, err := fs.filmRepository.GetAllFilms(ctx,nil)
+	if err != nil{
+		return []entity.Film{},err
+	}
+	return films,nil
+}
+
+func (fs *filmService) UpdateFilm(ctx context.Context,filmDTO dto.FilmUpdateRequest, slug string) (dto.FilmUpdateRequest, error){
+	film, err := fs.filmRepository.UpdateFilmBySlug(ctx,nil,slug,filmDTO)
+	if err != nil{
+		return dto.FilmUpdateRequest{},err
+	}
+	return film,nil
+}
