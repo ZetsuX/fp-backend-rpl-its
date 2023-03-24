75f583bc86082878a40e44ad1d0b2e35ba3c570c
Fehlling Halim <kevnhalim@gmail.com>
2023-03-25 00:41:28 +0700

    fix : entity attribute data types

--- entity/spot.go
+++ entity/spot.go
@@ -8,6 +8,6 @@ type Spot struct {
 	Number        int          `json:"number" binding:"required"`
 	SessionID     uint64       `gorm:"foreignKey" json:"session_id" binding:"required"`
 	Session       *Session     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"session,omitempty"`
-	TransactionID uint64       `gorm:"foreignKey" json:"transaction_id"`
+	TransactionID *uint64      `gorm:"foreignKey" json:"transaction_id"`
 	Transaction   *Transaction `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"transaction,omitempty"`
 }
