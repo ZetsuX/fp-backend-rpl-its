f4cacdb62a34e81a2f469a3c870f62def9349834
Fehlling Halim <kevnhalim@gmail.com>
2023-03-24 18:50:43 +0700

    feat : adding spot_per_row attribute to area

--- dto/area.go
+++ dto/area.go
@@ -1,6 +1,7 @@
 package dto
 
 type AreaCreateRequest struct {
-	Name      string `json:"name" binding:"required"`
-	SpotCount int    `json:"spot_count" binding:"required"`
+	Name       string `json:"name" binding:"required"`
+	SpotCount  int    `json:"spot_count" binding:"required"`
+	SpotPerRow int    `json:"spot_per_row" binding:"required"`
 }
--- entity/area.go
+++ entity/area.go
@@ -4,7 +4,8 @@ import "fp-rpl/common"
 
 type Area struct {
 	common.Model
-	Name      string    `json:"name" binding:"required"`
-	SpotCount int       `json:"spot_count" binding:"required"`
-	Sessions  []Session `json:"session,omitempty"`
+	Name       string    `json:"name" binding:"required"`
+	SpotCount  int       `json:"spot_count" binding:"required"`
+	SpotPerRow int       `json:"spot_per_row" binding:"required"`
+	Sessions   []Session `json:"session,omitempty"`
 }
