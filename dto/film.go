e3ba8824bd5d1d31679e86fa56fcbd299cff0f0b
Muhammad Hafidh Rosyadi <mhafidhrosyadi@gmail.com>
2023-03-26 05:50:02 +0700

    feat:fix UpdateFilmBySlug

--- dto/film.go
+++ dto/film.go
@@ -15,17 +15,3 @@ type FilmRegisterRequest struct {
 	Image      string    `json:"image" binding:"required"`
 	Status     string    `json:"status" binding:"required"`
 }
-type FilmUpdateRequest struct {
-	Title      string    `json:"title" binding:"required"`
-	Synopsis   string    `json:"synopsis" binding:"required"`
-	Duration   int       `json:"duration" binding:"required"`
-	Genre      string    `json:"genre" binding:"required"`
-	Producer   string    `json:"producer" binding:"required"`
-	Director   string    `json:"director" binding:"required"`
-	Writer     string    `json:"writer" binding:"required"`
-	Production string    `json:"production" binding:"required"`
-	Cast       string    `json:"cast" binding:"required"`
-	Trailer    string    `json:"trailer" binding:"required"`
-	Image      string    `json:"image" binding:"required"`
-	Status     string    `json:"status" binding:"required"`
-}
