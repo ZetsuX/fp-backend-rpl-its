f6b9d46c52d828eeeff57e6cfd5378ebf78b7b64
Fehlling Halim <kevnhalim@gmail.com>
2023-03-30 12:02:06 +0700

    refactor: removing unused timestamp field from transaction

--- dto/transaction.go
+++ dto/transaction.go
@@ -3,7 +3,6 @@ package dto
 type TransactionMakeRequest struct {
 	Code       string
 	TotalPrice float64
-	Timestamp  string   `json:"timestamp" binding:"required"`
 	SpotsName  []string `json:"spots_name" binding:"required"`
 	UserID     uint64
 	SessionID  uint64
--- entity/transaction.go
+++ entity/transaction.go
@@ -6,7 +6,6 @@ type Transaction struct {
 	common.Model
 	Code 	   string   `json:"code" binding:"required"`
 	TotalPrice float64  `json:"total_price" binding:"required"`
-	Timestamp  string   `json:"timestamp" binding:"required"`
 	Spots      []Spot   `json:"spot,omitempty"`
 	UserID     uint64   `gorm:"foreignKey" json:"user_id" binding:"required"`
 	User       *User    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"user,omitempty"`
